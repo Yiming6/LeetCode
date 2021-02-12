ded1f478ba3e2b577f6c9de30f050ae4e8a14b84
Yiming6 <eamonge.921212@gmail.com>
2021-02-12 16:19:28 -0500

    Create 965_Univalued Binary Tree.java
    Recursively traversal. Top to bottom.

---
+++ 965_Univalued Binary Tree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) return true;
+        if (root.left == null && root.right == null) return true;
+        
+        boolean left = root.left == null ? true : root.val == root.left.val;
+        boolean right = root.right == null ? true : root.val == root.right.val;
+        
+        return left && right && isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
+}
