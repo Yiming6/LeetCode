f1f1548e44b088272de8ca10451869d946a2f11c
Yiming6 <eamonge.921212@gmail.com>
2021-02-13 22:20:33 -0500

    Create 1104.Path In Zigzag Labelled Binary Tree.java

---
+++ 1104.Path In Zigzag Labelled Binary Tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    /*
+        Math problem.
+    */
+    public List<Integer> pathInZigZagTree(int label) {
+        LinkedList<Integer> result = new LinkedList<>();
+        int parent = label;
+        result.addFirst(parent);
+        
+        while (parent != 1) {
+            int d = (int)(Math.log(parent) / Math.log(2));
+            int offset = (int) (Math.pow(2, d + 1) - 1 - parent);
+            parent = ((int) Math.pow(2, d) + offset) / 2;
+            result.addFirst(parent);
+        }
+        
+        return result;
+    }
+}
