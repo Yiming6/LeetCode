e7633348cccd3fc7f7c057d18c711277710c960e
Yiming6 <eamonge.921212@gmail.com>
2021-02-16 20:59:28 -0500

    Create 1302.Deepest Leaves Sum.java

---
+++ 1302.Deepest Leaves Sum.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+        Level order traversal.
+    */
+    public int deepestLeavesSum(TreeNode root) {
+        if (root == null) return 0;
+        
+        List<List<Integer>> tree = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode curr = queue.poll();
+                list.add(curr.val);
+                if (curr.left != null) queue.add(curr.left);
+                if (curr.right != null) queue.add(curr.right);
+            }
+            tree.add(list);
+        }
+        
+        int ans = 0;
+        for (int i : tree.get(tree.size() - 1)) ans += i;
+        
+        return ans;
+    }
+}
