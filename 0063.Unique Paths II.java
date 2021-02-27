1116c8c283286475951e67d84d1c0070fd54e02b
Yiming6 <eamonge.921212@gmail.com>
2021-02-27 11:44:53 -0500

    Create 0063.Unique Paths II.java

---
+++ 0063.Unique Paths II.java
@@ -0,0 +1,36 @@
+class Solution {
+    /*
+        Same as question 62. Note the number path in the place of the obstacle is 0.
+    */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid.length == 0 || obstacleGrid[0].length == 0 || obstacleGrid[0][0] == 1) return 0;
+        
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                while (i < m) dp[i++][0] = 0;
+            } else dp[i][0] = 1;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                while (i < n) dp[0][i++] = 0;
+            } else dp[0][i] = 1;
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        
+        return dp[m - 1][n - 1];
+    }
+}
