2541066216aa5c26ea8b5a4893b6085679f99523
Yiming6 <eamonge.921212@gmail.com>
2021-03-09 18:34:34 -0500

    Create 0547.Number of Provinces.java

---
+++ 0547.Number of Provinces.java
@@ -0,0 +1,43 @@
+class Solution {
+    /*
+        dfs.
+        Detect how many partitions in the graph.
+        check each node, if it is not visited, dfs it and mark all reachable node to visited
+        count the number of unvisited parts.
+        O(n^2 + n);
+    */
+    public int findCircleNum(int[][] isConnected) {
+        if (isConnected.length == 0 || isConnected[0].length == 0) return 0;
+        
+        int n = isConnected.length;
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (isConnected[i][j] == 1) graph[i].add(j);
+            }
+        }
+        
+        boolean[] visited = new boolean[n];
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            
+            ans++;
+            dfs(graph, visited, i);
+        }
+        
+        return ans;
+    }
+    
+    private void dfs(List<Integer>[] graph, boolean[] visited, int curr) {
+        visited[curr] = true;
+        
+        for (int next : graph[curr]) {
+            if (!visited[next]) dfs(graph, visited, next);
+        }
+    }
+}
