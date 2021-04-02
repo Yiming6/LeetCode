d53bc92c0ba6c8cc6f2380b94f8c9dfa01906807
Yiming6 <eamonge.921212@gmail.com>
2021-04-01 22:59:03 -0400

    Update 0473.Matchsticks to Square.java

--- 0473.Matchsticks to Square.java
+++ 0473.Matchsticks to Square.java
@@ -1,6 +1,10 @@
 class Solution {
     /*
-        DFS, Recursion, Enumeration.
+        set a array with length of 4 to represent each side of square and find all 
+        possible combinations. Validate whether it is a square when at the last number.
+        To speed it up, we compare it with target which is the length of one side. If 
+        it is larger than the target value, we can omit it and check next.
+        O(n * 2^n).
     */
     public boolean makesquare(int[] nums) {
         if (nums.length < 4) return false;
@@ -10,28 +14,28 @@ class Solution {
         
         if (sum % 4 != 0) return false;
         
-        int[] sides = new int[]{0, 0, 0, 0};
-        int curr = 0, target = sum / 4;
+        int target = sum / 4;
+        int[] square = new int[4];
         
-        return dfs(nums, sides, curr, target);
+        return helper(nums, square, 0, target);
     }
     
-    private boolean dfs(int[] nums, int[] sides, int curr, int target) {
+    private boolean helper(int[] nums, int[] square, int curr, int target) {
         if (curr >= nums.length) return false;
         
-        for (int i = 0; i < sides.length; i++) {
-            if (sides[i] + nums[curr] > target) continue;
+        for (int i = 0; i < 4; i++) {
+            if (square[i] + nums[curr] > target) continue;
             
-            sides[i] += nums[curr];
+            square[i] += nums[curr];
             
             if (curr == nums.length - 1) {
-                if (sides[0] == target && sides[1] == target && sides[2] == target) 
+                if (square[0] == target && square[1] == target && square[2] == target)
                     return true;
             }
             
-            if (dfs(nums, sides, curr + 1, target)) return true;
+            if (helper(nums, square, curr + 1, target)) return true;
             
-            sides[i] -= nums[curr];
+            square[i] -= nums[curr];
         }
         
         return false;
