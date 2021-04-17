3d5f2b4649899ae84f21c22ca4a9342f86e5a93e
Yiming6 <eamonge.921212@gmail.com>
2021-04-17 01:14:21 -0400

    Update 0491.Increasing Subsequences.java

--- 0491.Increasing Subsequences.java
+++ 0491.Increasing Subsequences.java
@@ -1,29 +1,32 @@
 class Solution {
     /*
-        dfs to find subsequence. Similar to question 131. 
+        dfs to find subsequence. Enumerate all possible increasing subsequences.
+        Note to avoid duplicate element by using a hashset.
+        O(2^n)
     */
     public List<List<Integer>> findSubsequences(int[] nums) {
-        if (nums.length == 0) return new ArrayList<>();
+        if (nums.length < 2) return new ArrayList<>();
         
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         
-        dfs(nums, 0, list, ans);
+        helper(nums, 0, list, ans);
         
         return ans;
     }
     
-    private void dfs(int[] nums, int start, List<Integer> list, List<List<Integer>> ans) {
+    private void helper(int[] nums, int start, List<Integer> list, List<List<Integer>> ans) {
         if (list.size() >= 2) ans.add(new ArrayList<>(list));
         
-        
+
         Set<Integer> set = new HashSet<>();
         for (int i = start; i < nums.length; i++) {
-            if ((!list.isEmpty() && list.get(list.size() - 1) > nums[i]) || set.contains(nums[i])) continue;
+            if ((list.size() != 0 && nums[i] < list.get(list.size() - 1)) || set.contains(nums[i])) continue;
             
             set.add(nums[i]);
+            
             list.add(nums[i]);
-            dfs(nums, i + 1, list, ans);
+            helper(nums, i + 1, list, ans);
             list.remove(list.size() - 1);
         }
     }
