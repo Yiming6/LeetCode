4c3e2862096036f47918a08b6f48d26c4fdae2ec
Yiming6 <eamonge.921212@gmail.com>
2021-04-16 20:30:33 -0400

    Update 0490.The Maze.java

--- 0490.The Maze.java
+++ 0490.The Maze.java
@@ -2,32 +2,35 @@ class Solution {
     /*
         dfs. Find a target in graph.
     */
+    
+    int[][] dirs;
     public boolean hasPath(int[][] maze, int[] start, int[] destination) {
         if (maze.length == 0 || maze[0].length == 0) return false;
         
+        dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         int m = maze.length, n = maze[0].length;
         boolean[][] visited = new boolean[m][n];
         
-        return dfs(maze, start, destination, visited);
+        return dfs(maze, visited, start, destination);
     }
     
-    private boolean dfs(int[][] maze, int[] curr, int[] destination, boolean[][] visited) {
-        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int m = maze.length, n = maze[0].length, y = curr[0], x = curr[1];
+    private boolean dfs(int[][] maze, boolean[][] visited, int[] start, int[] destination) {
+        int m = maze.length, n = maze[0].length;
         
-        visited[y][x] = true;
+        visited[start[0]][start[1]] = true;
         
         for (int[] dir : dirs) {
-            y = curr[0]; x = curr[1];
-            while (y + dir[0] >= 0 && y + dir[0] < m && x + dir[1] >= 0 && x + dir[1] < n && maze[y + dir[0]][x + dir[1]] != 1) {
+            int y = start[0], x = start[1];
+            
+            while (y + dir[0] >= 0 && y + dir[0] < m && x + dir[1] >= 0 && x + dir[1] < n && maze[y + dir[0]][x + dir[1]] == 0) {
                 y += dir[0];
                 x += dir[1];
             }
             
-            if (y == destination[0] && x == destination[1]) return true;
+            if (destination[0] == y && destination[1] == x) return true;
             if (visited[y][x]) continue;
             
-            if (dfs(maze, new int[]{y, x}, destination, visited)) return true;
+            if (dfs(maze, visited, new int[]{y, x}, destination)) return true;
         }
         
         return false;
