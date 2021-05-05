8408c36f68a99d3e6b68e1a9759e27e533aa953b
Yiming6 <eamonge.921212@gmail.com>
2021-05-05 00:59:26 -0400

    Update 0529.Minesweeper.java

--- 0529.Minesweeper.java
+++ 0529.Minesweeper.java
@@ -1,28 +1,25 @@
 class Solution {
-    /*
-        Traversal Graph.
-    */
     
     int[][] dirs;
     public char[][] updateBoard(char[][] board, int[] click) {
         if (board.length == 0 || board[0].length == 0) return board;
         
-        dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {-1, -1}, {-1, 1}, {1, -1}};
-                
-        dfs(board, click[0], click[1]);
+        dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {1, 1}, {1, -1}, {-1, 1}};
+        int m = board.length, n = board[0].length;
+        
+        helper(board, click[0], click[1]);
         
         return board;
     }
     
-    private void dfs(char[][] board, int i, int j) {
+    private void helper(char[][] board, int i, int j) {
         int m = board.length, n = board[0].length;
         
         if (board[i][j] == 'M') {
             board[i][j] = 'X';
             return;
         }
-                
-        // check surrounding
+        
         int counter = 0;
         for (int[] dir : dirs) {
             int y = i + dir[0], x = j + dir[1];
@@ -34,11 +31,9 @@ class Solution {
         if (counter == 0) {
             board[i][j] = 'B';
             
-            // dfs surrounding
             for (int[] dir : dirs) {
                 int y = i + dir[0], x = j + dir[1];
-                if (y >= 0 && y < m && x >= 0 && x < n && board[y][x] == 'E') 
-                    dfs(board, y, x);
+                if (y >= 0 && y < m && x >= 0 && x < n && board[y][x] == 'E') helper(board, y, x);
             }
         } else board[i][j] = (char) (counter + '0');
     }
