4e969fcb14295ab94f83b2fbcce30493ce616ee8
Yiming6 <eamonge.921212@gmail.com>
2021-05-06 00:20:31 -0400

    531

--- 0531.Lonely Pixel I.java
+++ 0531.Lonely Pixel I.java
@@ -1,20 +1,17 @@
 class Solution {
-    /*
-        traversal graph.
-    */
     
     int[][] dirs;
     public int findLonelyPixel(char[][] picture) {
         if (picture.length == 0 || picture[0].length == 0) return 0;
         
-        int m = picture.length, n = picture[0].length, ans = 0;
         dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int m = picture.length, n = picture[0].length, ans = 0;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (picture[i][j] == 'W') continue;
-                
-                if (dfs(picture, i, j)) ans++;
+                if (picture[i][j] == 'B') {
+                    if (dfs(picture, i, j)) ans++;
+                }
             }
         }
         
@@ -25,11 +22,13 @@ class Solution {
         int m = picture.length, n = picture[0].length;
         
         for (int[] dir : dirs) {
-            int y = i + dir[0], x = j + dir[1];
-            while (y >= 0 && y < m && x >= 0 && x < n) {
-                if (picture[y][x] == 'B') return false;
+            int y = i, x = j;
+            
+            while (y + dir[0] >= 0 && y + dir[0] < m && x + dir[1] >= 0 && x + dir[1] < n) {
                 y += dir[0];
                 x += dir[1];
+                
+                if (picture[y][x] == 'B') return false;
             }
         }
         
