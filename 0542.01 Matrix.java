1839aa1377f4ba443da3d6c75a31725037f92a26
Yiming6 <eamonge.921212@gmail.com>
2021-05-07 00:25:50 -0400

    Update 0542.01 Matrix.java

--- 0542.01 Matrix.java
+++ 0542.01 Matrix.java
@@ -1,46 +1,33 @@
 class Solution {
-    /*
-        dfs traversal the graph. bfs is also working.
-        In the beginning, set all 1 into Integer.max value. Start traversal from 0 and 
-        initial distance is 0. If next grid is 0 continue.
-        If next grid is not 0, check its current number and compare with distance from 
-        starter. If current distance is smaller, update it and continue traversalling. 
-        Otherwise, stop searching.
-        O(kmn) k is the number of 0.
-    */
-    
-    int[][] dirs;
-    public int[][] updateMatrix(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) return new int[0][0];
+    public int[][] updateMatrix(int[][] mat) {
+        if (mat.length == 0 || mat[0].length == 0) return new int[0][0];
         
-        dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        int m = matrix.length, n = matrix[0].length;
+        int m = mat.length, n = mat[0].length;
+        int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) 
-                if (matrix[i][j] == 1) matrix[i][j] = Integer.MAX_VALUE;
-        }
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (matrix[i][j] != 0) continue;
-                
-                dfs(matrix, i, j, 0);
+                if (mat[i][j] == 0) queue.add(new Pair<>(i, j));
+                else mat[i][j] = Integer.MAX_VALUE;
             }
         }
         
-        return matrix;
-    }
-    
-    private void dfs(int[][] matrix, int i, int j, int dis) {
-        int m = matrix.length, n = matrix[0].length;
+        while (!queue.isEmpty()) {
+            Pair<Integer, Integer> curr = queue.poll();
+            
+            for (int[] dir : dirs) {
+                int y = curr.getKey() + dir[0], x = curr.getValue() + dir[1];
                 
-        for (int[] dir : dirs) {
-            int y = i + dir[0], x = j + dir[1];
-            if (y >= 0 && y < m && x >= 0 && x < n && matrix[y][x] > dis + 1) {
-                matrix[y][x] = dis + 1;
-                dfs(matrix, y, x, dis + 1);
+                if (y >= 0 && y < m && x >= 0 && x < n && mat[y][x] > mat[curr.getKey()][curr.getValue()] + 1) {
+                    mat[y][x] = mat[curr.getKey()][curr.getValue()] + 1;
+                    queue.add(new Pair<>(y, x));
+                }
+                
             }
         }
+        
+        return mat;
     }
 }
