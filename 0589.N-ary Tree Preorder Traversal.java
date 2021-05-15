026594ec4e0cb2ef88ce8dd088b4eadc36fc0eca
Yiming6 <eamonge.921212@gmail.com>
2021-05-15 13:12:43 -0400

    Create 0589.N-ary Tree Preorder Traversal.java

---
+++ 0589.N-ary Tree Preorder Traversal.java
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        if (root == null) return new ArrayList<>();
+        
+        List<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+        
+        return ans;
+    }
+    
+    private void helper(Node root, List<Integer> list) {
+        if (root == null) return;
+        
+        list.add(root.val);
+        for (Node child : root.children) helper(child, list);
+    }
+}
