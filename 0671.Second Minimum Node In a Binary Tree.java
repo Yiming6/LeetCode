082ed34446bb8bc7625baaf1e55ddcbdb2507fcd
Yiming6 <eamonge.921212@gmail.com>
2021-05-18 16:53:51 -0400

    Create 0671.Second Minimum Node In a Binary Tree.java

---
+++ 0671.Second Minimum Node In a Binary Tree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findSecondMinimumValue(TreeNode root) {
+        if (root.left == null) return -1;
+        
+        int left = (root.val == root.left.val) ? findSecondMinimumValue(root.left) : root.left.val;
+        int right = (root.val == root.right.val) ? findSecondMinimumValue(root.right) : root.right.val;
+        
+        return (left == -1 || right == -1) ? Math.max(left, right) : Math.min(left, right);
+    }
+}
