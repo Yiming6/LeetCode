1f6daeee70863f26d578e9ee80efb31c44991f78
Yiming6 <eamonge.921212@gmail.com>
2021-05-20 23:40:11 -0400

    Create 0744.Find Smallest Letter Greater Than Target.java

---
+++ 0744.Find Smallest Letter Greater Than Target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) left = mid + 1;
+            else right = mid;
+        }
+        
+        return right == letters.length ? letters[0] : letters[right];
+    }
+}
