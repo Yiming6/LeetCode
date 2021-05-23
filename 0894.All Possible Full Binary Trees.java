f7453f39d4897b52a1bda5055708be347543ab9a
Yiming6 <eamonge.921212@gmail.com>
2021-05-23 17:27:27 -0400

    Create 0894.All Possible Full Binary Trees.java

---
+++ 0894.All Possible Full Binary Trees.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> allPossibleFBT(int n) {
+        if (n % 2 == 0) return new ArrayList<>();
+        
+        List<TreeNode> ans = new ArrayList<>();
+
+        if (n == 1) {
+            ans.add(new TreeNode(0));
+            return ans;
+        }
+        
+        for (int i = 1; i < n - 1; i += 2) {
+            List<TreeNode> left = allPossibleFBT(i), right = allPossibleFBT(n - i - 1);
+            
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(0);
+                    
+                    root.left = l;
+                    root.right = r;
+                    
+                    ans.add(root);
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
