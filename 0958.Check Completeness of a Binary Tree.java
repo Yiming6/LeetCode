8a3af3b85a05521e1f6b276380faf8745e95d2af
Yiming6 <eamonge.921212@gmail.com>
2021-05-25 01:30:29 -0400

    Update 0958.Check Completeness of a Binary Tree.java

--- 0958.Check Completeness of a Binary Tree.java
+++ 0958.Check Completeness of a Binary Tree.java
@@ -14,43 +14,29 @@
  * }
  */
 class Solution {
-    /*
-        Level order traversal. Check completeness of last level and others separately.
-    */
     public boolean isCompleteTree(TreeNode root) {
-        if (root == null) return false;
-        if (root.left == null && root.right == null) return true;
+        if (root == null) return true;
         
-        // level order traversal
-        List<List<TreeNode>> tree = new ArrayList<>();
+        List<TreeNode> list = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
         
         queue.add(root);
         
         while (!queue.isEmpty()) {
-            List<TreeNode> list = new ArrayList<>();
-            for (int i = queue.size(); i > 0; i--) {
-                TreeNode curr = queue.poll();
-                list.add(curr);
-                if (curr.left != null) queue.add(curr.left);
-                if (curr.right != null) queue.add(curr.right);
-            }
-            tree.add(list);
+            TreeNode curr = queue.poll();
+            
+            list.add(curr);
+            
+            if (curr.left != null) queue.add(curr.left);
+            if (curr.right != null) queue.add(curr.right);
         }
         
-        // check the completeness except the last level
-        for (int i = 0; i < tree.size() - 1; i++) {
-            if (tree.get(i).size() != Math.pow(2, i)) return false;
-        }
-        
-        // check the completeness of the last level
-        List<TreeNode> list = tree.get(tree.size() - 2);
-        int index = list.size() - 1;
-        while (list.get(index).right == null && list.get(index).left == null) index--;
-        if (list.get(index).left == null && list.get(index).right != null) return false;
-        
-        for (int i = index - 1; i >= 0; i--) {
-            if (list.get(i).left == null || list.get(i).right == null) return false;
+        for (int i = 0; i < list.size(); i++) {
+            TreeNode curr = list.get(i);
+            
+            if ((i + 1) * 2 <= list.size() && (curr.left != list.get((i + 1) * 2 - 1))) return false;
+            if ((i + 1) * 2 + 1 <= list.size() && (curr.right != list.get((i + 1) * 2))) return false;
+            
         }
         
         return true;
