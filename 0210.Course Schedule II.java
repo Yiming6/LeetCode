0bd55f3bb440a4d7df327b0ca80ddef0f1d8ad8e
Yiming6 <eamonge.921212@gmail.com>
2021-05-26 01:01:01 -0400

    Update 0210.Course Schedule II.java

--- 0210.Course Schedule II.java
+++ 0210.Course Schedule II.java
@@ -1,49 +1,42 @@
 class Solution {
-    /*
-        Topological sorting, implemented by dfs. After one node's all paths are being 
-        detected and no cycle, we can add this node to list. When building the graph, because 
-        the directions is the same as orders of courses, the order that we added node
-        in list is opposited. 
-        O(E + V).
-    */
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         if (numCourses == 0) return new int[0];
         
-        List<Integer> list = new ArrayList<>();
         List<Integer>[] graph = new ArrayList[numCourses];
-        
         for (int i = 0; i < numCourses; i++) graph[i] = new ArrayList<>();
+        
         for (int[] prerequisite : prerequisites) {
             graph[prerequisite[1]].add(prerequisite[0]);
         }
         
-        // 0: unvisited, -1: processing, 1: processed
+        List<Integer> list = new ArrayList<>();
+        // 0: unvisited, 1: processed, -1: processing
         int[] visited = new int[numCourses];
         
         for (int i = 0; i < numCourses; i++) {
-            if (hasCycle(graph, visited, i, list)) return new int[0];
+            if (!dfs(graph, visited, i, list)) return new int[0];
         }
         
         Collections.reverse(list);
-        int[] ans = new int[numCourses];
         
+        int[] ans = new int[numCourses];
         for (int i = 0; i < numCourses; i++) ans[i] = list.get(i);
         
         return ans;
     }
     
-    private boolean hasCycle(List<Integer>[] graph, int[] visited, int curr, List<Integer> list) {
-        if (visited[curr] != 0) return visited[curr] == -1;
+    private boolean dfs(List<Integer>[] graph, int[] visited, int curr, List<Integer> list) {
+        if (visited[curr] != 0) return visited[curr] == 1;
         
         visited[curr] = -1;
         
         for (int next : graph[curr]) {
-            if (hasCycle(graph, visited, next, list)) return true;
+            if (!dfs(graph, visited, next, list)) return false;
         }
         
-        visited[curr] = 1;
         list.add(curr);
-
-        return false;
+        visited[curr] = 1;
+        
+        return true;
     }
 }
